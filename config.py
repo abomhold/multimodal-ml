17694ea7f2b1b095dbf8f54c8f0b8f92bf86b8bf
austin <austinbomhold@gmail.com>
2024-10-09 15:20:17 -0700

    corrections

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
-INPUT_PATH = "training/profile/profile.csv"
-OUTPUT_PATH = "output"
+INPUT_PATH: str
+OUTPUT_PATH: str
+
+
+def get_configs():
+    print(f"input path: {INPUT_PATH}")
+    print(f"output path: {OUTPUT_PATH}")
+
+
+if __name__ == '__main__':
+    get_configs()
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import sys
+import config
 import pre_processing as pre
-from config import *
+
 
 def main():
+    config.INPUT_PATH = sys.argv[1]
+    config.OUTPUT_PATH = sys.argv[2]
     pre.build_baseline()
 
 
 if __name__ == "__main__":
-    print(sys.argv)
-    INPUT_PATH = sys.argv[1]
-    OUTPUT_PATH = sys.argv[2]
-    print(INPUT_PATH, OUTPUT_PATH)
     main()
-
