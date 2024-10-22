7376363c941dafe3e018f5f3248184393aedcc43
Masumi Yano <masumiyano0706@gmail.com>
2024-10-22 16:55:23 -0700

    update for cnn gender

--- image/image_utils.py
+++ image/image_utils.py
@@ -3,7 +3,7 @@ import os
 from PIL import Image
 import pandas as pd
 import torch
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader, TensorDataset, random_split
 
 
 def get_image(image_dir):
@@ -25,7 +25,7 @@ def get_classes(class_path):
     return classes
 
 
-def create_dataloader(images, classes, batch_size=32):
+def create_dataset(images, classes, batch_size=32):
     image_tensors = []
     class_tensors = []
     for user_id, image_tensor in images.items():
@@ -36,6 +36,19 @@ def create_dataloader(images, classes, batch_size=32):
     class_tensors = torch.tensor(class_tensors)
 
     dataset = TensorDataset(image_tensors, class_tensors)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-    return dataloader
+    return dataset
+
+
+def split_train_val_dataset(dataset, train_size=0.8, batch_size=32):
+    dataset_size = len(dataset)
+    train_len = int(train_size*dataset_size)
+    val_len = dataset_size - train_len
+
+    train_dataset, val_dataset = random_split(dataset, [train_len, val_len])
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader
+
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import sys
-import config 
+import config
 import pre_processing as pre
 import image.image_testrun as image_testrun
 import torch
 
-
-device = "gpu" if torch.cuda.is_available() else "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def main():
-    #config.INPUT_PATH = sys.argv[1]
-    #config.OUTPUT_PATH = sys.argv[2]
-    #pre.build_baseline()
+    config.INPUT_PATH = sys.argv[1]
+    config.OUTPUT_PATH = sys.argv[2]
+    pre.build_baseline()
 
-    image_testrun.train(config.IMAGE_TRAIN_PATH, config.CLASS_TRAIN_PATH, device)
+    image_testrun.test(config.IMAGE_TEST_PATH, config.CLASS_TEST_PATH, device)
+
 
 if __name__ == "__main__":
     main()
