ca10e8ab293578080b25b822694954abf4f97aca
austin <austinbomhold@gmail.com>
2024-10-22 23:41:59 -0700

    writing xmls

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from pathlib import Path
 import config
 import text.preprocessing
 import preprocessing as pre
-
+import postprocessing as post
 
 import image.image_testrun as image_testrun
 import torch
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+# device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # def collate_data():
 
@@ -22,10 +22,13 @@ def main():
     data = pre.main(input_path.joinpath(config.PROFILE_PATH))
 
     # Text preprocessing
-    data = text.preprocessing.main(input_path.joinpath(config.TEXT_DIR), data)
-    print(data)
+    # data = text.preprocessing.main(input_path.joinpath(config.TEXT_DIR), data)
+    # print(data)
 
-    result = image_testrun.test(input_path.joinpath(config.IMAGE_DIR), data, device)
+    # result = image_testrun.test(config.IMAGE_TEST_PATH, data, device)
+
+    post.write_xml(data)
+
 
 
 if __name__ == "__main__":
---
+++ postprocessing.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def write_xml(data: pd.DataFrame):
+    print(data.head())
