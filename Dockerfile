b074fa2944eb457b7c28cd4681427a4589eab70f
austin <austinbomhold@gmail.com>
2024-10-31 02:21:23 -0700

    Austin GRADED

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM python:3.12.7-bookworm
 LABEL authors="austin"
 COPY . /home
 WORKDIR /home
-RUN pip3 install -r requirements.txt
+#RUN #pip3 install -r requirements.txt
 ENTRYPOINT ["python3", "/home/main.py"]
 #ENTRYPOINT ["bash"]
--- main.py
+++ main.py
@@ -1,15 +1,12 @@
+import subprocess
 import sys
-from pathlib import Path
-# import pandas as pd
-import config
-import text.main as text
-import image
-import like
-import preprocessing as pre
-import postprocessing as post
 
-import image.image_testrun as image_testrun
+
 import torch
+import config
+import postprocessing as post
+import preprocessing as pre
+import text.main as text
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model_name = 'resnet50'
@@ -20,11 +17,13 @@ def main():
     data = pre.main()
     config.get_configs()
     text_df = text.main(config.TEXT_DIR, data.copy())
-    #image_df = image_testrun.test(config.IMAGE_DIR, data.copy(), model_name, device)
-    #like_df = data
-    #combined_df = post.majority(text_df, image_df, like_df)
+    # image_df = image_testrun.test(config.IMAGE_DIR, data.copy(), model_name, device)
+    # like_df = data
+    # combined_df = post.majority(text_df, image_df, like_df)
     post.write_xml(config.OUTPUT_PATH, text_df)
     print("Done!")
 
+
 if __name__ == "__main__":
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
     main()
