ff12524c20ebb835fdee752af6c3249efdfe6fb5
austin <austinbomhold@gmail.com>
2024-11-02 12:36:03 -0700

    updates

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 model_name = 'resnet50'
 
 
-# LEAVE THIS UNTIL AFTER 0900, I need to be graded
 def main():
     print("Starting...")
     data = pre.main()
     config.get_configs()
-    text_df = text.main(config.TEXT_DIR, data.copy())
-    # image_df = image_testrun.test(config.IMAGE_DIR, data.copy(), model_name, device)
-    # like_df = data
-    # combined_df = post.majority(text_df, image_df, like_df)
-    post.write_xml(config.OUTPUT_PATH, text_df)
+    print(data)
+    # text_df = text.main(config.TEXT_DIR, data.copy())
+    # # image_df = image_testrun.test(config.IMAGE_DIR, data.copy(), model_name, device)
+    # # like_df = data
+    # # combined_df = post.majority(text_df, image_df, like_df)
+    # post.write_xml(config.OUTPUT_PATH, text_df)
     print("Done!")
 
 
