2180ff5dc2f5538085e7b1d5917b1ec09d5531b0
austin <austinbomhold@gmail.com>
2024-11-03 10:36:44 -0800

    austin GRADED

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ model_name = 'resnet50'
 def main():
     print("Starting...")
     data = pre.main()
-    text_df = text.main(config.TEXT_DIR, data.copy())
+    #text_df = text.main(config.TEXT_DIR, data.copy())
     #image_df = image_testrun.test(config.IMAGE_DIR, data.copy(), model_name, device)
-    #like_df = like.bayes.predict_gender(config.LIKE_DIR, data.copy())
+    like_df = like.bayes.predict_gender(config.LIKE_DIR, data.copy())
     #combined_df = post.majority(text_df, image_df, like_df)
-    post.write_xml(config.OUTPUT_PATH, text_df)
+    post.write_xml(config.OUTPUT_PATH, like_df)
     print("Done!")
 
 
