2219f029c608fd65ba8c5ef4d2cb6247b82d7164
austin <austinbomhold@gmail.com>
2024-11-11 22:02:41 -0800

    encoding sensitive text processing

--- text/emojis.py
+++ text/emojis.py
@@ -1,194 +1,85 @@
-EMOTICONS = {
+emoticons = {
     # Happy faces
-    ":-)": "Smiley",
-    ":)": "Smiley",
-    ":-]": "Smiley",
-    ":]": "Smiley",
-    ":->": "Smiley",
-    ":>": "Smiley",
-    "8-)": "Smiley",
-    "8)": "Smiley",
-    ":-}": "Smiley",
-    ":}": "Smiley",
-    ":^)": "Smiley",
-    "=]": "Smiley",
-    "=)": "Smiley",
+    tuple([":-))", ":))", ":-)", ":)", ":-]", ":]", ":->", ":>", "8-)", "8)", ":-}", ":}", ":^)", "=]", "=)"]):
+        "Smiley",
 
     # Laughing
-    ":-D": "Laughing",
-    ":D": "Laughing",
-    "8-D": "Laughing",
-    "8D": "Laughing",
-    "=D": "Laughing",
-    "=3": "Laughing",
-    "B^D": "Laughing",
-    "c:": "Laughing",
-    "C:": "Laughing",
+    tuple([":-D", ":D", "8-D", "8D", "=D", "=3", "B^D", "c:", "C:", "x-D", "xD", "X-D", "XD"]):
+        "Laughing",
 
-    # XD Laughing
-    "x-D": "Laughing",
-    "xD": "Laughing",
-    "X-D": "Laughing",
-    "XD": "Laughing",
+    # Crying/Happy tears
+    tuple([":'-(", ":'(", ":=("]): "Crying",
+    tuple([":'-)", ":')", ":\"D"]): "Happy",
 
-    # Very happy
-    ":))": "Happy",
-    ":-))": "Happy",
-
-    # Sad
-    ":-(": "Frown",
-    ":(": "Frown",
-    ":-c": "Frown",
-    ":c": "Frown",
-    ":-<": "Frown",
-    ":<": "Frown",
-    ":-[": "Frown",
-    ":[": "Frown",
-    ":-||": "Frown",
-    ":{": "Frown",
-    ":@": "Frown",
-    ";(": "Frown",
-
-    # Crying
-    ":'-(": "Crying",
-    ":'(": "Crying",
-    ":=(": "Crying",
-
-    # Happy tears
-    ":')": "Happy",
-    ":'-)": "Happy",
-    ":\"D": "Happy",
+    # Sad/Frown
+    tuple([":-(", ":(", ":-c", ":c", ":-<", ":<", ":-[", ":[", ":-||", ":{", ":@", ";("]):
+        "Frown",
 
     # Angry
-    ">:(": "Angry",
-    ">:[": "Angry",
+    tuple([">:(", ">:["]): "Angry",
 
     # Horror
-    "D-':": "Horror",
-    "D:<": "Horror",
-    "D:": "Horror",
-    "D8": "Horror",
-    "D;": "Horror",
-    "D=": "Horror",
-    "DX": "Horror",
+    tuple(["D-':", "D:<", "D:", "D8", "D;", "D=", "DX"]):
+        "Horror",
 
     # Surprise
-    ":-O": "Surprise",
-    ":O": "Surprise",
-    ":-o": "Surprise",
-    ":o": "Surprise",
-    ":-0": "Surprise",
-    ":0": "Surprise",
-    "8-0": "Surprise",
-    ">:O": "Surprise",
-    "=O": "Surprise",
-    "=o": "Surprise",
-    "=0": "Surprise",
+    tuple([":-O", ":O", ":-o", ":o", ":-0", ":0", "8-0", ">:O", "=O", "=o", "=0"]):
+        "Surprise",
 
-    # Cat
-    ":-3": "Cat",
-    ":3": "Cat",
-    "=3": "Cat",
-    "x3": "Cat",
-    "X3": "Cat",
-    ">:3": "Lion",
+    # Cat faces
+    tuple([":-3", ":3", "=3", "x3", "X3"]): "Cat",
+    tuple([">:3"]): "Lion",
 
     # Kiss
-    ":-*": "Kiss",
-    ":*": "Kiss",
-    ":x": "Kiss",
+    tuple([":-*", ":*", ":x"]): "Kiss",
 
     # Wink
-    ";-)": "Wink",
-    ";)": "Wink",
-    "*-)": "Wink",
-    "*)": "Wink",
-    ";-]": "Wink",
-    ";]": "Wink",
-    ";^)": "Wink",
-    ";>": "Wink",
-    ":-,": "Wink",
-    ";D": "Wink",
-    ";3": "Wink",
+    tuple([";-)", ";)", "*-)", "*)", ";-]", ";]", ";^)", ";>", ":-,", ";D", ";3"]):
+        "Wink",
 
     # Tongue
-    ":-P": "Tongue",
-    ":P": "Tongue",
-    "X-P": "Tongue",
-    "XP": "Tongue",
-    "x-p": "Tongue",
-    ":p": "Tongue",
-    ":b": "Tongue",
-    "d:": "Tongue",
-    "=p": "Tongue",
-    ">:P": "Tongue",
+    tuple([":-P", ":P", "X-P", "XP", "x-p", ":p", ":b", "d:", "=p", ">:P", ":-J"]):
+        "Tongue",
 
     # Skeptical
-    ":-/": "Skeptical",
-    ":/": "Skeptical",
-    ":-.": "Skeptical",
-    ">=\\": "Skeptical",
-    ">:/": "Skeptical",
-    ":\\": "Skeptical",
-    "=/": "Skeptical",
-    "=\\": "Skeptical",
-    ":L": "Skeptical",
-    "=L": "Skeptical",
-    ":S": "Skeptical",
+    tuple([":-/", ":/", ":-.", ">=\\", ">:/", ":\\", "=/", "=\\", ":L", "=L", ":S", "',:-|", "',:-l"]):
+        "Skeptical",
 
     # Straight face
-    ":-|": "Straight",
-    ":|": "Straight",
+    tuple([":-|", ":|"]): "Straight",
 
     # Embarrassed
-    ":$": "Embarrassed",
-    "://)": "Embarrassed",
-    "://3": "Embarrassed",
+    tuple([":$", "://)", "://3"]): "Embarrassed",
 
-    # Sealed
-    ":-X": "Sealed",
-    ":X": "Sealed",
-    ":-#": "Sealed",
-    ":#": "Sealed",
-    ":-&": "Sealed",
-    ":&": "Sealed",
+    # Sealed lips
+    tuple([":-X", ":X", ":-#", ":#", ":-&", ":&"]): "Sealed",
 
     # Angel
-    "O:-)": "Angel",
-    "O:)": "Angel",
-    "0:-3": "Angel",
-    "0:3": "Angel",
-    "0:-)": "Angel",
-    "0:)": "Angel",
-    "0;^)": "Angel",
+    tuple(["O:-)", "O:)", "0:-3", "0:3", "0:-)", "0:)", "0;^)"]):
+        "Angel",
 
     # Evil
-    ">:-)": "Evil",
-    ">:)": "Evil",
-    "}:-)": "Evil",
-    "}:)": "Evil",
-    "3:-)": "Evil",
-    "3:)": "Evil",
-    ">;-)": "Evil",
-    ">;)": "Evil",
+    tuple([">:-)", ">:)", "}:-)", "}:)", "3:-)", "3:)", ">;-)", ">;)"]):
+        "Evil",
 
-    # Miscellaneous
-    "|;-)": "Cool",
-    "|-O": "Cool",
-    "B-)": "Cool",
-    ":-J": "Tongue",
-    "#-)": "Partied",
-    "%-)": "Drunk",
-    "%": "Drunk",
-    ":-###..": "Sick",
-    ":###..": "Sick",
-    "<:-|": "Dumb",
-    "',:-|": "Skeptical",
-    "',:-l": "Skeptical",
-    ":E": "Grimacing",
-    "8-X": "Skull",
-    "8=X": "Skull",
-    "x-3": "Skull",
-    "x=3": "Skull",
-    "~:>": "Chicken"
+    # Cool/Bored
+    tuple(["|;-)", "|-O", "B-)"]): "Cool",
+
+    # Drunk
+    tuple(["%-)", "%"]): "Drunk",
+
+    # Sick
+    tuple([":-###..", ":###.."]): "Sick",
+
+    # Dumb
+    tuple(["<:-|"]): "Dumb",
+
+    # Grimacing
+    tuple([":E"]): "Grimacing",
+
+    # Skull
+    tuple(["8-X", "8=X", "x-3", "x=3"]): "Skull",
+
+    # Chicken
+    tuple(["~:>"]): "Chicken"
 }
