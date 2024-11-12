169a86e2d02dc0a06e817dd7ab558d996297a530
austin <austinbomhold@gmail.com>
2024-11-11 22:02:36 -0800

    encoding sensitive text processing

--- text/clean.py
+++ text/clean.py
@@ -7,46 +7,32 @@ import nltk
 import pandas as pd
 from nltk import word_tokenize
 
+import text.emojis as emojis_py
+
+emojis: Dict[str, str] = emojis_py.EMOTICONS
+
 # Ensure necessary downloads for nltk
 nltk.download(['punkt', 'punkt_tab', 'stopwords', 'wordnet'], quiet=True)
-
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
 lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
 
-happy = [":‑)", ":)", ":-]", ":]", ":->", ":>", "8-)", "8)", ":-}", ":}", ":^)", "=]", "=)", ":‑D", ":D", "8‑D", "8D",
-         "=D", "=3", "B^D", "c:", "C:", "x‑D", "xD", "X‑D", "XD", ":-))", "^:))"]
-sad = [":‑(", ":(", ":‑c", ":c", ":‑ < ", ": < ", ":‑[", ": [", ":- | | ", ": {", ": @ ", ":(", ";(", ":'‑(", ":'(",
-       " := ("]
-
 
 def clean_text(text: str) -> str:
     # text = unicodedata.normalize('NFD', text)
     text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
-    text = re.sub('|'.join(map(re.escape, happy)), ' happy ', text)
-    text = re.sub('|'.join(map(re.escape, sad)), ' sad ', text)
+    text =
+    text = re.sub('|'.join(map(re.escape, key)), f" {emojis[key]} emoji", text)
+
     text = re.sub(r'[^a-zA-Z\s]', ' ', text, flags=re.MULTILINE)
     text = re.sub(r'(\w)(\1{2,})', lambda m: m.group(1) + m.group(1), text, flags=re.MULTILINE)
     text = re.sub(r'\s+', ' ', text)
 
     words = [lemmatizer.lemmatize(word) for word in word_tokenize(text) if word.lower() not in stop_words]
-
     return ' '.join(words).lower()
 
-
-# def process_text_files(text_path: Path) -> (Dict[str, str], Dict[str, str]):
-#     user_texts = {}
-#     user_words = {}
-#     for file_path in text_path.glob('*.txt'):
-#         user_id = file_path.stem
-#         with file_path.open('r', encoding='utf-8', errors='ignore') as text_file:
-#             content = text_file.read()
-#             user_words[user_id] = clean_text(content)
-#             user_texts[user_id] = content
-#     return user_texts, user_words
-#
 
 def process_text_files(text_path: Path) -> (Dict[str, str], Dict[str, str]):
     user_texts = {}
@@ -72,12 +58,19 @@ def process_text_files(text_path: Path) -> (Dict[str, str], Dict[str, str]):
 
 
 def main(path: Path, data: pd.DataFrame):
+    # Read text files into a dictionary
     user_texts, user_words = process_text_files(path)
+    print(user_words)
+    # Transform the dictionary into a DataFrame
+    # For original text:
     text_df = pd.DataFrame.from_dict(user_texts, orient='index', columns=['text'])
     text_df.index.name = 'userid'
+    # For cleaned text:
     word_df = pd.DataFrame.from_dict(user_words, orient='index', columns=['words'])
     word_df.index.name = 'userid'
+    # Combine text and word DataFrames
     text_df = text_df.join(word_df, how='inner', on='userid')
+    # Combine text DataFrame with original data
     data.set_index('userid', inplace=True)
     combined_df = data.join(text_df, how='inner', on='userid')
     combined_df.reset_index(inplace=True)
---
+++ text/emojis.py
@@ -0,0 +1,194 @@
+EMOTICONS = {
+    # Happy faces
+    ":-)": "Smiley",
+    ":)": "Smiley",
+    ":-]": "Smiley",
+    ":]": "Smiley",
+    ":->": "Smiley",
+    ":>": "Smiley",
+    "8-)": "Smiley",
+    "8)": "Smiley",
+    ":-}": "Smiley",
+    ":}": "Smiley",
+    ":^)": "Smiley",
+    "=]": "Smiley",
+    "=)": "Smiley",
+
+    # Laughing
+    ":-D": "Laughing",
+    ":D": "Laughing",
+    "8-D": "Laughing",
+    "8D": "Laughing",
+    "=D": "Laughing",
+    "=3": "Laughing",
+    "B^D": "Laughing",
+    "c:": "Laughing",
+    "C:": "Laughing",
+
+    # XD Laughing
+    "x-D": "Laughing",
+    "xD": "Laughing",
+    "X-D": "Laughing",
+    "XD": "Laughing",
+
+    # Very happy
+    ":))": "Happy",
+    ":-))": "Happy",
+
+    # Sad
+    ":-(": "Frown",
+    ":(": "Frown",
+    ":-c": "Frown",
+    ":c": "Frown",
+    ":-<": "Frown",
+    ":<": "Frown",
+    ":-[": "Frown",
+    ":[": "Frown",
+    ":-||": "Frown",
+    ":{": "Frown",
+    ":@": "Frown",
+    ";(": "Frown",
+
+    # Crying
+    ":'-(": "Crying",
+    ":'(": "Crying",
+    ":=(": "Crying",
+
+    # Happy tears
+    ":')": "Happy",
+    ":'-)": "Happy",
+    ":\"D": "Happy",
+
+    # Angry
+    ">:(": "Angry",
+    ">:[": "Angry",
+
+    # Horror
+    "D-':": "Horror",
+    "D:<": "Horror",
+    "D:": "Horror",
+    "D8": "Horror",
+    "D;": "Horror",
+    "D=": "Horror",
+    "DX": "Horror",
+
+    # Surprise
+    ":-O": "Surprise",
+    ":O": "Surprise",
+    ":-o": "Surprise",
+    ":o": "Surprise",
+    ":-0": "Surprise",
+    ":0": "Surprise",
+    "8-0": "Surprise",
+    ">:O": "Surprise",
+    "=O": "Surprise",
+    "=o": "Surprise",
+    "=0": "Surprise",
+
+    # Cat
+    ":-3": "Cat",
+    ":3": "Cat",
+    "=3": "Cat",
+    "x3": "Cat",
+    "X3": "Cat",
+    ">:3": "Lion",
+
+    # Kiss
+    ":-*": "Kiss",
+    ":*": "Kiss",
+    ":x": "Kiss",
+
+    # Wink
+    ";-)": "Wink",
+    ";)": "Wink",
+    "*-)": "Wink",
+    "*)": "Wink",
+    ";-]": "Wink",
+    ";]": "Wink",
+    ";^)": "Wink",
+    ";>": "Wink",
+    ":-,": "Wink",
+    ";D": "Wink",
+    ";3": "Wink",
+
+    # Tongue
+    ":-P": "Tongue",
+    ":P": "Tongue",
+    "X-P": "Tongue",
+    "XP": "Tongue",
+    "x-p": "Tongue",
+    ":p": "Tongue",
+    ":b": "Tongue",
+    "d:": "Tongue",
+    "=p": "Tongue",
+    ">:P": "Tongue",
+
+    # Skeptical
+    ":-/": "Skeptical",
+    ":/": "Skeptical",
+    ":-.": "Skeptical",
+    ">=\\": "Skeptical",
+    ">:/": "Skeptical",
+    ":\\": "Skeptical",
+    "=/": "Skeptical",
+    "=\\": "Skeptical",
+    ":L": "Skeptical",
+    "=L": "Skeptical",
+    ":S": "Skeptical",
+
+    # Straight face
+    ":-|": "Straight",
+    ":|": "Straight",
+
+    # Embarrassed
+    ":$": "Embarrassed",
+    "://)": "Embarrassed",
+    "://3": "Embarrassed",
+
+    # Sealed
+    ":-X": "Sealed",
+    ":X": "Sealed",
+    ":-#": "Sealed",
+    ":#": "Sealed",
+    ":-&": "Sealed",
+    ":&": "Sealed",
+
+    # Angel
+    "O:-)": "Angel",
+    "O:)": "Angel",
+    "0:-3": "Angel",
+    "0:3": "Angel",
+    "0:-)": "Angel",
+    "0:)": "Angel",
+    "0;^)": "Angel",
+
+    # Evil
+    ">:-)": "Evil",
+    ">:)": "Evil",
+    "}:-)": "Evil",
+    "}:)": "Evil",
+    "3:-)": "Evil",
+    "3:)": "Evil",
+    ">;-)": "Evil",
+    ">;)": "Evil",
+
+    # Miscellaneous
+    "|;-)": "Cool",
+    "|-O": "Cool",
+    "B-)": "Cool",
+    ":-J": "Tongue",
+    "#-)": "Partied",
+    "%-)": "Drunk",
+    "%": "Drunk",
+    ":-###..": "Sick",
+    ":###..": "Sick",
+    "<:-|": "Dumb",
+    "',:-|": "Skeptical",
+    "',:-l": "Skeptical",
+    ":E": "Grimacing",
+    "8-X": "Skull",
+    "8=X": "Skull",
+    "x-3": "Skull",
+    "x=3": "Skull",
+    "~:>": "Chicken"
+}
